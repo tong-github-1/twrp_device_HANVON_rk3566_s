1b8a7e9fb828d2e2a1927863197f62e6f0db8b0a
tong-github-1 <115772494+tong-github-1@users.noreply.github.com>
2026-07-06 10:30:04 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -21,3 +21,9 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/prebuilt/kernel:$(TARGET_COPY_OUT_RECOVERY)/kernel
 
 PRODUCT_POST_PROCESS += device/YUNLIANSHE/rk3566_s/hooks/post_dtb_copy.sh
+# 生成 dtb.img 以满足 recovery 依赖（相对路径，避开空变量）
+out/target/product/rk3566_s/dtb.img: $(LOCAL_PATH)/prebuilt/dtb.img
+	$(copy-file-to-target)
+
+# 强制 recovery 依赖此 dtb.img
+INSTALLED_DTBIMAGE_TARGET := out/target/product/rk3566_s/dtb.img
